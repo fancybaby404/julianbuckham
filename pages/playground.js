6e1567985b3fb70d834892b991f1c9cfcd1cfbe5
fancybaby404 <53817791+fancybaby404@users.noreply.github.com>
2022-11-02 19:47:38 +0800

    playground changes

--- pages/playground.js
+++ pages/playground.js
@@ -29,15 +29,15 @@ function Cube () {
 export default function Playground () {
 	return (
 		<div className="flex">
-			<div style={{"width": "20vw", "height": "100vh"}} className="flex content-center justify-center h-full align-middle flex-wrap bg-yellow-400">
-				<h1 className="text-9xl flex justify-center">yes.</h1>
-			</div>
 			<div style={{"backgroundColor": "black", "width": "80vw", "height": "100vh", "pointerEvents": "none"}}>
 				<Canvas>  
 						<ambientLight intensity={1}/>
 						<pointLight position={[1, 1, 1]}/>
 						<Cube/>
 				</Canvas>
+			</div>
+			<div style={{"width": "20vw", "height": "100vh"}} className="flex content-center justify-center h-full align-middle flex-wrap bg-yellow-400">
+				<h1 className="text-9xl flex justify-center">yes.</h1>
 			</div>
 		</div>
 
